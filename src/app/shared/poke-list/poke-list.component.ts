4790fa6c73c18829f4bbe9c49b2a7d9ad57587f9
Gustavo Nascimento <gusnas@alu.ufc.br>
2023-04-18 09:59:01 -0300

    adding details and error treatment

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
 import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
-  styleUrls: ['./poke-list.component.scss']
+  styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
-
+  private setAllPokemons: any;
   public getAllPokemons: any;
-  
-  constructor(
-    private pokeApiService: PokeApiService
-  ) { }
+
+  public apiError: boolean = false;
+
+  constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
-      res => {
-        this.getAllPokemons = res.results;
+      (res) => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      },
+      error => {
+        this.apiError = true;
       }
     );
   }
 
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
+  }
 }
